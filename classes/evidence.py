a0215e4b812dc66387133c50289fb6e764c4ea15
Nael <naelassouma2@gmail.com>
2026-05-14 15:38:08 +0100

    Added NPC classes

--- classes/evidence.py
+++ classes/evidence.py
@@ -9,19 +9,6 @@ class Evidence(InteractableObject):
     '''
     A piece of evidence that the player can collect and view in the evidence bag.
     It inherits from InteractableObject.
-
-    Methods:
-        Evidence.draw(): draw evidence object and examination '[E] ...' prompt on a surface
-
-        Inherit methods from Interactable object
-
-    Attribute:
-        Evidence.collected (bool): is true if evidence is collected by player, false otherwise
-
-        Evidence.visible (bool): is evidence should be drawn to screen, false otherwise
-    
-        Inherit attribute from InteractableObject
-
     '''
     def __init__(self, room_name: str, name: str, obj_data: dict) -> None:
         super().__init__(room_name, name, obj_data)
@@ -33,5 +20,14 @@ class Evidence(InteractableObject):
         '''draw the evidence onto the given surface. also draws the [E] prompt if the player is near and has not collected it yet.'''
         if self.visible and not self.collected:
             super().draw(surface, player_center)
+
+class NPC(InteractableObject):
+    '''
+    A non-player character that the player can interact with by pressing E when nearby.
+    The player can also drop evidence on him to get a reaction.
+    inherits from InteractableObject because it also needs a sprite and rectangle for drawing and collision.
+    '''
+    def __init__(self, room_name: str, name: str, obj_data: dict) -> None:
+        super().__init__(room_name, name, obj_data)
 
 ### Nael Karimou - 5734316
