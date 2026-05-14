be5e4013829b5a77cb4adcf8487b52cb3cd14866
Amir Hossein Javadi Baygi <amirhossein.javadib@gmail.com>
2026-05-14 16:46:31 +0100

    UPDATE

--- classes/room_graph.py
+++ classes/room_graph.py
@@ -66,7 +66,7 @@ class RoomGraph:
         for adjacent in room.connections:
             self._unlock_edge(room, adjacent)
 
-    def _is_locked(self, room_a: rm.Room, room_b: rm.Room) -> bool:
+    def is_locked(self, room_a: rm.Room, room_b: rm.Room) -> bool:
         """returning True if the edge between two rooms is currently locked."""
         self._room_validate(room_a, room_b)
         self._edge_validate((room_a, room_b))
@@ -76,7 +76,7 @@ class RoomGraph:
         """returning True if all the edges of a room is currently locked."""
         self._room_validate(room)
         for adjacent in room.connections:
-            if not self._is_locked(room , adjacent):
+            if not self.is_locked(room , adjacent):
                 return False
         return True
 
@@ -113,7 +113,7 @@ class RoomGraph:
                 is_found = True
                 break
             for room in self.__graph[current_room]:
-                if room not in child_parent and not(respect_locks and self._is_locked(current_room, room)):
+                if room not in child_parent and not(respect_locks and self.is_locked(current_room, room)):
                     queue.append(room)
                     child_parent[room] = current_room
 
